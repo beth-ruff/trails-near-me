4b5ae08bf85ab580bfb8f25fafa562010fcbf0d8
github username <github email address>
2020-03-05 18:35:26 -0800

    add cli file, require cli file

--- lib/trails_near_me.rb
+++ lib/trails_near_me.rb
@@ -1,4 +1,8 @@
+# environment file
+# In here loads all files needed to run our app
+
 require "trails_near_me/version"
+require "trails_near_me/cli"
 
 module TrailsNearMe
   class Error < StandardError; end
